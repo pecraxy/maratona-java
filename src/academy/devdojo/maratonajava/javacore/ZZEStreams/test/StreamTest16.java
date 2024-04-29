9d16baad84d4ed5ecf90169e337819c0c61ef4b3
pecraxy <bemerda@hotmail.com>
2024-04-29 16:56:30 -0300

    Avanço até a aula 219..

--- src/academy/devdojo/maratonajava/javacore/ZZEStreams/test/StreamTest16.java
+++ src/academy/devdojo/maratonajava/javacore/ZZEStreams/test/StreamTest16.java
@@ -6,15 +6,17 @@ import academy.devdojo.maratonajava.javacore.ZZEStreams.dominio.Promotion;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class StreamTest16 {
     public static void main(String[] args) {
         System.out.println(Runtime.getRuntime().availableProcessors());
-        long num = 100_000_000;
+        long num = 10_000_000;
         sumStreamIterate(num);
-
         sumParallelStreamIterate(num);
+        sumLongStreamIterate(num);
+        sumParallelLongStreamIterate(num);
     }
 
     private static void sumStreamIterate(long num){
@@ -27,9 +29,25 @@ public class StreamTest16 {
 
     //
     private static void sumParallelStreamIterate(long num){
-        System.out.println("Sum Iterate");
+        System.out.println("Sum  Parallel Iterate");
         long init = System.currentTimeMillis();
         long result = Stream.iterate(1L, i -> i + 1).limit(num).parallel().reduce(0L, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - init) + "ms");
+    }
+
+    private static void sumLongStreamIterate(long num){
+        System.out.println("Sum LongStream Iterate");
+        long init = System.currentTimeMillis();
+        long result = LongStream.rangeClosed(1L, num).reduce(0, Long::sum);
+        long end = System.currentTimeMillis();
+        System.out.println(result + " " + (end - init) + "ms");
+    }
+
+    private static void sumParallelLongStreamIterate(long num){
+        System.out.println("Sum Long Parallel");
+        long init = System.currentTimeMillis();
+        long result = LongStream.rangeClosed(1L, num).parallel().reduce(0, Long::sum);
         long end = System.currentTimeMillis();
         System.out.println(result + " " + (end - init) + "ms");
     }
